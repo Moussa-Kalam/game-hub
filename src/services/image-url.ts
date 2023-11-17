7fcae61af7b6d3163b97a873cb8ef737d1ec76c7
Moussa-Kalam <m.amzat@alustudent.com>
2023-11-17 20:31:29 +0200

    feat: get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+const getCroppedImageUrl = (url: string) => {
+  // Get the index of the end of media/ in the url
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+
+  // Add the measure for the cropping to the url
+  return `${url.slice(0, index)}crop/600/400/${url.slice(index)}`;
+};
+
+export default getCroppedImageUrl;
