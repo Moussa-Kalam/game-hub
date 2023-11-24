4d658d64eba88256e2a6f8028c8a1355329c327c
Moussa-Kalam <m.amzat@alustudent.com>
2023-11-24 20:37:20 +0200

    feat: handle game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   // Get the index of the end of media/ in the url
   const target = "media/";
   const index = url.indexOf(target) + target.length;
