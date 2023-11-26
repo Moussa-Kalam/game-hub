548487015e131eb24eddaa1151e4ab9545376e3f
Moussa-Kalam <m.amzat@alustudent.com>
2023-11-26 02:39:09 +0200

    refactor: add early return in the game grid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,36 +13,35 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  return (
-    <>
-      {error && <Text>{error}</Text>}
-      <ul>
-        <SimpleGrid
-          columns={{
-            sm: 1,
-            md: 2,
-            lg: 3,
-            xl: 4,
-          }}
-          padding='10px'
-          spacing={6}
-        >
-          {/* Display the skeleton while it is loading */}
-          {isLoading &&
-            skeletons.map((skeleton) => (
-              <GameCardContainer key={skeleton}>
-                <GameCardSkeleton />
-              </GameCardContainer>
-            ))}
+  if (error) return <Text>{error}</Text>;
 
-          {data.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
+  return (
+    <ul>
+      <SimpleGrid
+        columns={{
+          sm: 1,
+          md: 2,
+          lg: 3,
+          xl: 4,
+        }}
+        padding='10px'
+        spacing={6}
+      >
+        {/* Display the skeleton while it is loading */}
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        </SimpleGrid>
-      </ul>
-    </>
+
+        {data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </ul>
   );
 };
 
