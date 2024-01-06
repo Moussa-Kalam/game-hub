95aba572fd349d8f14ad2808d3aad46943380fde
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-07 01:22:35 +0200

    fix: build issue solved

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APICLIENT from "../services/api-client";
 
-const apiClient = new APICLIENT<genres>("/genres");
+const apiClient = new APICLIENT<Genre>("/genres");
 
 export interface Genre {
   id: number;
