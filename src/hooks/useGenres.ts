bdcf636688fa736ae45bc12e4fecf44701e135c8
Moussa-Kalam <m.amzat@alustudent.com>
2024-03-03 01:13:24 +0200

    build: fix build issues

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
-import APICLIENT from "../services/api-client";
+import { useQuery } from '@tanstack/react-query';
+import genres from '../data/genres';
+import APICLIENT from '../services/api-client';
 
-const apiClient = new APICLIENT<Genre>("/genres");
+const apiClient = new APICLIENT<Genre>('/genres');
 
 export interface Genre {
   id: number;
@@ -12,7 +12,7 @@ export interface Genre {
 
 const useGenres = () =>
   useQuery({
-    queryKey: ["genres"],
+    queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
     initialData: { count: genres.length, results: genres },
